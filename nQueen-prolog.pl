f634fc70fdf4a0aee95d95555bef6785248abbfe
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2023-05-02 00:05:11 +0530

    prolog commit

--- nQueen-prolog.pl
+++ nQueen-prolog.pl
@@ -1,27 +1,27 @@
-% Predicate to solve N-Queens problem
-n_queens(N, Solution) :-
-    length(Solution, N),     % The solution list must have N elements
-    place_queens(N, Solution),    % Place the queens on the board
-    is_valid(Solution).      % Check if the solution is valid
+:- initialization(queens).
+queens(N, Queens) :-
+    length(Queens, N),
+	board(Queens, Board, 0, N, _, _),
+	queens(Board, 0, Queens).
 
-% Helper predicate to place the queens on the board
-place_queens(0, _).
-place_queens(I, Solution) :-
-    I > 0,
-    place_queens(I-1, Solution),
-    member(X, [1,2,3,4,5,6,7,8]),
-    no_attack(X/I, Solution),
-    Solution = [X | Rest].
+board([], [], N, N, _, _).
+board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
+	Col is Col0+1,
+	functor(Vars, f, N),
+	constraints(N, Vars, VR, VC),
+	board(Queens, Board, Col, N, VR, [_|VC]).
 
-% Helper predicate to check if the solution is valid
-is_valid([]).
-is_valid([X | Rest]) :-
-    no_attack(X/1, Rest),
-    is_valid(Rest).
+constraints(0, _, _, _) :- !.
+constraints(N, Row, [R|Rs], [C|Cs]) :-
+	arg(N, Row, R-C),
+	M is N-1,
+	constraints(M, Row, Rs, Cs).
 
-% Helper predicate to check if a queen can be placed in a given position
-no_attack(_, []).
-no_attack(X/Y, [X1/Y1 | Rest]) :-
-    Y =\= Y1,
-    abs(X1 - X) =\= abs(Y1 - Y),
-    no_attack(X/Y, Rest).
+queens([], _, []).
+queens([C|Cs], Row0, [Col|Solution]) :-
+	Row is Row0+1,
+	select(Col-Vars, [C|Cs], Board),
+	arg(Row, Vars, Row-Row),
+	queens(Board, Row, Solution).
+	
+ // to run code  in terminal queens(8, Queens).
